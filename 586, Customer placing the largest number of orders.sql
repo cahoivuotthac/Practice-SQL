367b0e094256a5198d315c3ec34c2df59c027094
Hien Phan <118596936+cahoivuotthac@users.noreply.github.com>
2023-12-04 01:05:25 +0700

    Create 586, Customer placing the largest number of orders.sql

---
+++ 586, Customer placing the largest number of orders.sql
@@ -0,0 +1,9 @@
+--who has the most order times--
+select customer_number
+from Orders 
+group by customer_number
+having count(order_number) >= all(
+    select count(order_number)
+    from Orders
+    group by customer_number
+)
